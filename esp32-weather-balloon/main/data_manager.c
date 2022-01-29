c739da7044e5cc088af147c6c4712a9c73d9a646
Erik Coltey <colteye@gmail.com>
2022-01-28 20:50:58 -0500

    data collection system almost fully works!

--- esp32-weather-balloon/main/data_manager.c
+++ esp32-weather-balloon/main/data_manager.c
@@ -23,7 +23,8 @@
 
 #define DATA_MANAGER_STACK_SIZE 4096
 
-xQueueHandle sstv_transmit_queue;
+uint8_t sstv_transmit_status;
+xSemaphoreHandle sstv_transmit_mutex;
 
 // Initialize radio waveform buffer.
 esp_err_t waveform_buffer_init(radio_waveform_data_t *waveform, uint32_t len)
@@ -57,42 +58,78 @@ void waveform_buffer_deinit(radio_waveform_data_t *waveform)
     free(waveform->buffer);
 }
 
-/*static void data_manager_ax25_task_entry(void *arg)
+void data_manager_ax25_task_entry(void *arg)
 {
+    // Init error.
+    esp_err_t err = ESP_OK;
+
+    // Create full AX25 buffer struct.
     radio_transmitter_config_t *radio_config = (radio_transmitter_config_t *)arg;
     uint8_t ax25_seconds = 5;
+    uint32_t ax25_buf_len = (radio_config->sample_rate * radio_config->bits_per_sample * ax25_seconds) / BYTE_SIZE;
 
-    uint8_t *ax25_buf_ptr;
-    uint8_t ax25_buf_len = (radio_config->sample_rate * radio_config->bits_per_sample * ax25_seconds) / BYTE_SIZE;
+    radio_waveform_data_t ax25_buf = {.current_osc = 0.25,
+                                      .current_transmit_bit = 0xFFFFFFFF,
+                                      .current_offset_bit = 0,
 
-    buffer_init(ax25_buf_ptr, ax25_buf_len);
+                                      .clip_seconds = ax25_seconds,
+                                      .buffer_bits_len = ax25_buf_len * BYTE_SIZE,
+                                      .buffer = NULL};
 
-    radio_waveform_data_t ax25_buf = {current_osc = 0.25,
-                                      current_transmit_bit = 0xFFFFFFFF,
-                                      current_offset_bit = 0,
-
-                                      clip_seconds = ax25_seconds,
-                                      buffer_bits_len = radio_config->sample_rate *
-                                                        radio_config->bits_per_sample *
-                                                        ax25_seconds,
-                                      buffer = ax25_buf_ptr};
-
-    QueueHandle_t *waveform_queue = radio_transmitter_get_waveform_queue();
-
-    // Initialize buffer and camera!
-    // Try to redo but if it takes too long, abort.
-    ESP_ERROR_REDO_VALIDATE("SSTV Buffer Initialize", buffer_init(sstv_buf_ptr, sstv_buf_len), 1000, 16);
-    ESP_ERROR_REDO_VALIDATE("SSTV Camera Initialize", camera_init, 1000, 16);
+    // Initialize buffer.
+    ESP_ERROR_TASK_VALIDATE("AX25 Buffer Initialize", err, waveform_buffer_init(&ax25_buf, ax25_buf_len))
 
     i2c_master_init();
     gps_init();
     mpu6050_init();
     bmp280_init();
+    gps_data_out_t gps_data;
+    bmp280_data_out_t bmp280_data;
+    mpu6050_data_out_t mpu6050_data;
 
+    // Get waveform queue and mutex from radio transmitter.
+    xSemaphoreHandle radio_busy_mutex = radio_transmitter_get_mutex();
+    xQueueHandle waveform_queue = radio_transmitter_get_waveform_queue();
+
+    uint8_t current_sstv_transmit_status = 1;
     for (;;)
     {
+        // Read SSTV transmit status controlled by semaphore.
+        while (xSemaphoreTake(sstv_transmit_mutex, pdMS_TO_TICKS(1000)) == pdFALSE)
+            ;
+        current_sstv_transmit_status = sstv_transmit_status;
+        xSemaphoreGive(sstv_transmit_mutex);
+
+        // If not transmitting, read data!
+        if (!current_sstv_transmit_status)
+        {
+            ESP_LOGI("MPU6050 Read", "TEMP");
+            mpu6050_read(&mpu6050_data);
+            ESP_LOGI("MPU6050 Read", "TEMP: %.6f", mpu6050_data.temperature);
+            ESP_LOGI("MPU6050 Read", "ROTATION X: %.6f", mpu6050_data.gyro_x);
+            ESP_LOGI("MPU6050 Read", "ROTATION Y: %.6f", mpu6050_data.gyro_y);
+            ESP_LOGI("MPU6050 Read", "ROTATION Z: %.6f\n", mpu6050_data.gyro_z);
+            ESP_LOGI("MPU6050 Read", "ACCEL X: %.6f", mpu6050_data.acc_x);
+            ESP_LOGI("MPU6050 Read", "ACCEL Y: %.6f", mpu6050_data.acc_y);
+            ESP_LOGI("MPU6050 Read", "ACCEL Z: %.6f\n", mpu6050_data.acc_z);
+
+            bmp280_read(&bmp280_data);
+            ESP_LOGI("BMP280 Read", "TEMP: %.6f", bmp280_data.temperature);
+            ESP_LOGI("BMP280 Read", "PRESSURE: %.6f", bmp280_data.pressure);
+            ESP_LOGI("BMP280 Read", "ALTITUDE: %.6f\n", bmp280_data.altitude);
+
+            gps_read(&gps_data);
+            ESP_LOGI("GPS Read", "LATITUDE: %.6f", gps_data.latitude);
+            ESP_LOGI("GPS Read", "LONGITUDE: %.6f", gps_data.longitude);
+            ESP_LOGI("GPS Read", "ALTITUDE: %.6f", gps_data.altitude);
+            ESP_LOGI("GPS Read", "SPEED: %.6f\n", gps_data.speed);
+        }
+
+        // Add a bit of delay between each measurement.
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-}*/
+    vTaskDelete(NULL);
+}
 
 void data_manager_sstv_task_entry(void *arg)
 {
@@ -124,7 +161,12 @@ void data_manager_sstv_task_entry(void *arg)
     for (;;)
     {
         // Send to other tasks that sstv is not transmitting.
-        //xQueueSend(sstv_transmit_queue, (void *)0, pdMS_TO_TICKS(200));
+
+        // Set SSTV transmit status to 0, notify other tasks.
+        while (xSemaphoreTake(sstv_transmit_mutex, pdMS_TO_TICKS(1000)) == pdFALSE)
+            ;
+        sstv_transmit_status = 0;
+        xSemaphoreGive(sstv_transmit_mutex);
 
         // Reset SSTV buffer variables and take a picture!
         sstv_buf.current_osc = 0.25;
@@ -143,8 +185,13 @@ void data_manager_sstv_task_entry(void *arg)
         ESP_LOGI(SSTV_TAG, "Generating SCOTTIE 1 SSTV Waveform...");
         ESP_ERROR_CONTINUE_VALIDATE("SSTV Generate Waveform", err, 0, sstv_generate_waveform(pic, &sstv_buf))
 
-        // Send to other tasks that sstv is transmitting.
-        //xQueueSend(sstv_transmit_queue, (void *)1, pdMS_TO_TICKS(200));
+        // Set SSTV transmit status to 1, notify other tasks.
+        // Set SSTV transmit status to 0, notify other tasks.
+
+        while (xSemaphoreTake(sstv_transmit_mutex, pdMS_TO_TICKS(1000)) == pdFALSE)
+            ;
+        sstv_transmit_status = 1;
+        xSemaphoreGive(sstv_transmit_mutex);
 
         // Wait until semaphore can be taken.
         // Nothing else should be playing!
@@ -173,8 +220,10 @@ esp_err_t data_manager_init()
     // Init error.
     esp_err_t err = ESP_OK;
 
-    // Queue to send SSTV transmit state changes to other data writing tasks.
-    sstv_transmit_queue = xQueueCreate(1, sizeof(uint8_t));
+    // Mutex to read or modify SSTV transmit state.
+    sstv_transmit_status = 0;
+    sstv_transmit_mutex = xSemaphoreCreateMutex();
+    xSemaphoreGive(sstv_transmit_mutex);
 
     // Initialize radio system.
     radio_transmitter_config_t radio_config = {.pwm_range_addr = PWM_RANGE_ADDR,
@@ -194,6 +243,18 @@ esp_err_t data_manager_init()
 
     // Create data collection tasks.
     xReturned = xTaskCreatePinnedToCore(
+        data_manager_ax25_task_entry,
+        "AX25 Sensor Data Management",
+        DATA_MANAGER_STACK_SIZE,
+        (void *)radio_transmitter_get_config(),
+        tskIDLE_PRIORITY,
+        &xAX25Handle,
+        1);
+
+    if (xReturned != pdPASS)
+        return ESP_ERR_NO_MEM;
+
+    xReturned = xTaskCreatePinnedToCore(
         data_manager_sstv_task_entry,           // Function that implements the task.
         "SSTV Camera Data Management",          // Text name for the task.
         DATA_MANAGER_STACK_SIZE,                // Stack size in words, not bytes.
@@ -205,18 +266,6 @@ esp_err_t data_manager_init()
     if (xReturned != pdPASS)
         return ESP_ERR_NO_MEM;
 
-    /* Create the task, storing the handle. 
-    xReturned = xTaskCreate(
-        data_manager_ax25_task_entry,
-        "AX25 Sensor Data Management",
-        DATA_MANAGER_STACK_SIZE,
-        (void *)radio_transmitter_get_config(),
-        tskIDLE_PRIORITY,
-        &xAX25Handle);
-
-        if (xReturned != pdPASS)
-            return ESP_ERR_NO_MEM;
-            */
     return ESP_OK;
 }
 
